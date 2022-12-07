0ac494f190e365e784dcd097b5a0fd00d397cbd3
irynaprofile <105556785+irynaprofile@users.noreply.github.com>
2022-12-07 14:40:21 -0600

    add components

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Photos and Images';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://images.pexels.com/photos/1424687/pexels-photo-1424687.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+  image3 = 'https://images.pexels.com/photos/4200202/pexels-photo-4200202.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
 
   constructor() { }
 
